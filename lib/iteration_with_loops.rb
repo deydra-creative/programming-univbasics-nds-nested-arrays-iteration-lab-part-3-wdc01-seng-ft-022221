3a9ab9ab259cf97d4ce21ca02f0651ddbfc7ef08
Deydra Cavazos <deydra@cavazospartners.com>
2021-02-08 04:12:05 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,5 +1,9 @@
 def join_nested_strings(src)
-  
+  nested_strings = []
+  row_index = 0 
+  while row_index < src.count do 
+    element_index = 0 
+    while element_index <src[row_index]
 end
  
  
